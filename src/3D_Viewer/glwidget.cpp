2d0494f8a25f127df71ad8f388b439f5bbbe15e1
finchren <finchren@student.21-school.ru>
2023-04-03 19:53:17 +0300

    More comments - forget what each fucntion does

--- src/3D_Viewer/glwidget.cpp
+++ src/3D_Viewer/glwidget.cpp
@@ -13,10 +13,10 @@ const double cubeVertices[] = {
 };
 
 // Index data for the cube lines
-const GLuint cubeIndices[] = {
+const unsigned int cubeIndices[] = {
     0, 1, 1, 2, 2, 3, 3, 0, // Front face
     4, 5, 5, 6, 6, 7, 7, 4, // Back face
-    0, 4, 1, 5, 2, 6, 3, 7  // Connecting lines
+    0, 4, 1, 5, 2, 6, 3, 7  // Connecting front and back
 };
 
 GLWidget::GLWidget(QWidget *parent)
@@ -27,8 +27,9 @@ GLWidget::GLWidget(QWidget *parent)
 void GLWidget::initializeGL()
 {
     initializeOpenGLFunctions();
-
+    // Enable the use of vertex arrays for drawing
     glEnableClientState(GL_VERTEX_ARRAY);
+    // Specify the format and the location of the vertex data in the array
     glVertexPointer(3, GL_DOUBLE, 0, cubeVertices);
 }
 
@@ -37,12 +38,13 @@ void GLWidget::paintGL()
     // Set background color
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
+    // Clear the color buffer and depth buffer
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Draw the cube points
+    // Draw the points
     glDrawArrays(GL_POINTS, 0, sizeof(cubeVertices) / (3 * sizeof(double)));
 
-    // Draw the cube lines
+    // Draw the lines
     glDrawElements(GL_LINES, sizeof(cubeIndices) / sizeof(GLuint), GL_UNSIGNED_INT, cubeIndices);
 }
 
