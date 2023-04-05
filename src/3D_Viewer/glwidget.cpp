514bd70ebd58ee7e40e0a04678752442831e2fe2
finchren <finchren@student.21-school.ru>
2023-04-05 19:11:51 +0300

    Basic parsing of face-style obj file done, projection of the matrix set

--- src/3D_Viewer/glwidget.cpp
+++ src/3D_Viewer/glwidget.cpp
@@ -1,27 +1,7 @@
 #include "glwidget.h"
 
-
 double cubeVertices[24];
-unsigned int cubeIndices[24];
-
-// Vertex data for the cube
-//const double cubeVertices[] = {
-//    -0.5, -0.5, -0.5,
-//     0.5, -0.5, -0.5,
-//     0.5,  0.5, -0.5,
-//    -0.5,  0.5, -0.5,
-//    -0.5, -0.5,  0.5,
-//     0.5, -0.5,  0.5,
-//     0.5,  0.5,  0.5,
-//    -0.5,  0.5,  0.5
-//};
-
-// Index data for the cube lines
-//const unsigned int cubeIndices[] = {
-//    0, 1, 1, 2, 2, 3, 3, 0, // Front face
-//    4, 5, 5, 6, 6, 7, 7, 4, // Back face
-//    0, 4, 1, 5, 2, 6, 3, 7  // Connecting front and back
-//};
+unsigned int cubeIndices[72];
 
 void parseObjFile(const char *filename) {
     int vertexIndex = 0;
@@ -42,11 +22,30 @@ void parseObjFile(const char *filename) {
             cubeVertices[vertexIndex++] = x;
             cubeVertices[vertexIndex++] = y;
             cubeVertices[vertexIndex++] = z;
-        } else if (line[0] == 'l' && line[1] == ' ') {
+        }
+        // Parse the line-style obj file
+          else if (line[0] == 'l' && line[1] == ' ') {
             int indices[2];
             sscanf(line, "l %d %d", &indices[0], &indices[1]);
             cubeIndices[faceIndex++] = indices[0] - 1;
             cubeIndices[faceIndex++] = indices[1] - 1;
+        }
+        // Parse the face-style obj file
+          else if (line[0] == 'f' && line[1] == ' ') {
+            int indices[3];
+            sscanf(line, "f %d %d %d", &indices[0], &indices[1], &indices[2]);
+            for (int i = 0; i < 3; i++) {
+                // Convert to zero-based index
+                --indices[i];
+            }
+            // Triangulate and convert to line segments
+            cubeIndices[faceIndex * 6] = indices[0];
+            cubeIndices[faceIndex * 6 + 1] = indices[1];
+            cubeIndices[faceIndex * 6 + 2] = indices[1];
+            cubeIndices[faceIndex * 6 + 3] = indices[2];
+            cubeIndices[faceIndex * 6 + 4] = indices[2];
+            cubeIndices[faceIndex * 6 + 5] = indices[0];
+            faceIndex++;
         }
     }
     fclose(file);
@@ -55,13 +54,11 @@ void parseObjFile(const char *filename) {
     }
 }
 
-
-
-
 GLWidget::GLWidget(QWidget *parent)
     : QOpenGLWidget(parent)
 {
-    parseObjFile("/home/finchren/school/s21_3DViewer/src/3D_Viewer/line_cube.obj");
+//    parseObjFile("/home/finchren/school/s21_3DViewer/src/3D_Viewer/line_cube.obj");
+    parseObjFile("/home/finchren/school/s21_3DViewer/src/3D_Viewer/cube.obj");
 }
 
 void GLWidget::initializeGL()
@@ -75,19 +72,41 @@ void GLWidget::initializeGL()
 
 void GLWidget::paintGL()
 {
-    // Set background color
+    // Set background color: RGB and opacity
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-    // Clear the color buffer and depth buffer
+    // Clear the color buffer (color values of the pixels displayed on the screen)
+    // and depth buffer (distance of each pixel)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-//    parseObjFile("/home/finchren/school/s21_3DViewer/src/3D_Viewer/cube.obj");
+    // Set up the projection matrix
+    // QMatrix4x4 is a data type that represents a 4x4 matrix, used for transformations in 3D graphics
+    QMatrix4x4 projection;
+    // First value - field of view angle in degrees. Determines how much of the scene is visible
+    // Second value - aspect ratio of the screen. Ensures that the scene is displayed properly on screens of different sizes and aspect ratios
+    // Third value - distance from the viewer to the near clipping plane, which determines how close objects can be to the viewer before they are clipped (not rendered)
+    // Forth value - distance from the viewer to the far clipping plane, which determines how far objects can be from the viewer before they are clipped
+    // Clipping is necessary because the graphics pipeline can only render objects that are within a certain range of distances from the viewer, and any objects that fall outside of this range are clipped or removed from the scene before rendering.
+    projection.perspective(50.0, (double)width() / height(), 0.1, 100.0);
+
+    // Set up the model-view matrix
+    QMatrix4x4 modelView;
+    // First set of values - position of the camera
+    // Second set of values - position of the center
+    // Third set of values - determines the orientation of the camera properly (in relation to the ground and other objects in the scene)
+    modelView.lookAt(QVector3D(3, 3, 5), QVector3D(0, 0, 0), QVector3D(0, 1, 0));
+
+    // Load the projection and model-view matrices
+    glMatrixMode(GL_PROJECTION);
+    glLoadMatrixf(projection.constData());
+    glMatrixMode(GL_MODELVIEW);
+    glLoadMatrixf(modelView.constData());
 
     // Draw the points
     glDrawArrays(GL_POINTS, 0, sizeof(cubeVertices) / (3 * sizeof(double)));
 
     // Draw the lines
-    glDrawElements(GL_LINES, sizeof(cubeIndices) / sizeof(GLuint), GL_UNSIGNED_INT, cubeIndices);
+    glDrawElements(GL_LINES, sizeof(cubeIndices) / sizeof(unsigned int), GL_UNSIGNED_INT, cubeIndices);
 }
 
 void GLWidget::resizeGL(int width, int height)
